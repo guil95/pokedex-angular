947dffdce5619ffe5447f2109063c4ab5807ef4d
Guilherme Rodrigues <guilherme.rodrigues@sabium.com.br>
2018-02-23 09:26:33 -0300

    pokedex

--- src/app/services/pokedexapi.service.ts
+++ src/app/services/pokedexapi.service.ts
@@ -12,7 +12,7 @@ export class PokedexApiService{
 constructor(
     private httpClient: HttpClient
 ){}
-    public url_api: string = 'https://cors.now.sh/https://pokeapi.co/api/v2/'
+    public url_api: string = 'https://pokeapi.co/api/v2/'
 
     public buscarPokemons<Pokemon>():Observable<Pokemon> {
         return this.httpClient.get<Pokemon>(this.url_api+'pokemon?limit=721')
